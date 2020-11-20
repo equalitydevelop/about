542123df0c8af25498ce96ed7608005249209790
lst <Leona.Strucelj@cenosco.com>
2020-11-20 02:02:55 +0100

    Revert fixed form

--- public/assets/mail/contact_me.js
+++ public/assets/mail/contact_me.js
@@ -2,75 +2,6 @@ $(function () {
     $(
         "#contactForm input,#contactForm textarea,#contactForm button"
     ).jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(" ") >= 0) {
-                firstName = name.split(" ").slice(0, -1).join(" ");
-            }
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $.ajax({
-                url: "/assets/mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                },
-                cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                error: function () {
-                    // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                },
-            });
-        },
         filter: function () {
             return $(this).is(":visible");
         },
@@ -86,3 +17,77 @@ $(function () {
 $("#name").focus(function () {
     $("#success").html("");
 });
+
+window.addEventListener("DOMContentLoaded", function() {    
+    var form = document.getElementById("contactForm");
+    var button = document.getElementById("sendMessageButton");
+    var status = document.getElementById("my-form-status");
+    
+    function success() {
+        $("#success").html("<div class='alert alert-success'>");
+        $("#success > .alert-success")
+            .html(
+                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append("</button>");
+        $("#success > .alert-success").append(
+            "<strong>Your message has been sent. </strong>"
+        );
+        $("#success > .alert-success").append("</div>");
+        //clear all fields
+        $("#contactForm").trigger("reset");
+    }
+
+    function error() {
+        $("#success").html("<div class='alert alert-danger'>");
+        $("#success > .alert-danger")
+            .html(
+                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append("</button>");
+        $("#success > .alert-danger").append(
+            $("<strong>").text(
+                "Sorry " +
+                form.elements[0].value +
+                    ", you have to fill in all the fields. Please try again!"
+            )
+        );
+        $("#success > .alert-danger").append("</div>");
+        //clear all fields
+        $("#contactForm").trigger("reset");
+    }
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+
+        var data = {
+            name: form.elements[0].value,
+            email: form.elements[1].value,
+            phone: form.elements[2].value,
+            message: form.elements[3].value,
+        };
+        const isEmpty = Object.values(data).some(x => (x === null || x === ''));
+        if(isEmpty)
+         data = {};
+        var myJSON = JSON.stringify(data);
+
+      ajax(form.method, form.action, myJSON, success, error);
+    });
+  });
+  
+
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
